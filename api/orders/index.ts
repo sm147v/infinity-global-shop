bffd268abf642f57341aa1dfc6aeb0e18193bbe9
vlbaa23 <vlbaa23@gmail.com>
2026-04-29 01:07:27 -0500

    Fix: tipo explicito en orders index

--- api/orders/index.ts
+++ api/orders/index.ts
@@ -28,7 +28,7 @@ export async function createOrderFromPayload(payload: unknown) {
     throw new ApiError("Some products do not exist", 400);
   }
 
-  const map = new Map(products.map((product) => [product.id, product]));
+const map = new Map(products.map((product: any) => [product.id, product]));
 
   const orderItems = data.items.map((item) => {
     const product = map.get(item.productId);
