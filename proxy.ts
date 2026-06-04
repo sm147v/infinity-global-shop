25ef2d53005218145a30e3a9de70e90ccc5dd534
sm147v <sm.147v@gmail.com>
2026-06-04 13:36:48 -0500

    feat: agregar cabecera HSTS para forzar HTTPS

--- proxy.ts
+++ proxy.ts
@@ -12,6 +12,7 @@ function addSecurityHeaders(response: NextResponse): NextResponse {
   response.headers.set("X-Frame-Options", "DENY");
   response.headers.set("X-XSS-Protection", "1; mode=block");
   response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
+  response.headers.set("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
   return response;
 }
 
